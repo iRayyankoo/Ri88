ce3a65dc9513a1d1f20ba3288cd5d4a6f403de0c
irayy <irayy@example.com>
2026-02-23 06:22:37 +0300

    Fix: ensure api/auth/signout redirects to new domain www.ri88.info

--- src/app/api/auth/signout/route.ts
+++ src/app/api/auth/signout/route.ts
@@ -1,8 +1,9 @@
 import { signOut } from "@/auth";
 
 export async function GET() {
-    await signOut({ redirectTo: "/" });
+    await signOut({ redirectTo: "https://www.ri88.info" });
 }
+
 export async function POST() {
-    await signOut({ redirectTo: "/" });
+    await signOut({ redirectTo: "https://www.ri88.info" });
 }
